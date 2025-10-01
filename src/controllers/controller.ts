715c841619fb34b4a072826d9a870a0e2108ee26
NicoleGluj <nicolegluj@gmail.com>
2025-09-30 21:43:37 -0300

    add: function main

---
+++ src/controllers/controller.ts
@@ -0,0 +1,7 @@
+
+
+const main = (accion: string) => {
+  console.log(accion)
+}
+
+export default main;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import main from "./controllers/controller"
+
+const argumentos = process.argv
+const accion = argumentos[2]
+
+main(accion)
