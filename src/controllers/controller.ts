e93584b2ea49da6ee4864ff8925246ea95921f2c
NicoleGluj <nicolegluj@gmail.com>
2025-09-30 21:51:37 -0300

    add: cases function main

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,7 +1,26 @@
 
 
 const main = (accion: string) => {
-  console.log(accion)
+  switch (accion) {
+    case "info":
+      console.log("Buscando informacion...")
+      break;
+    case "agregarProducto":
+      console.log("Agregando producto...")
+      break;
+    case "mostrarProductos":
+      console.log("Mostrando productos...")
+      break;
+    case "buscarProducto":
+      console.log("Buscando producto...")
+      break;
+    case "actualizarProducto":
+      console.log("Actualizando producto...")
+      break;
+    case "eliminarProducto":
+      console.log("Eliminando producto...")
+      break;
+  }
 }
 
 export default main;
